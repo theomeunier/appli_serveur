25663a461c3a9488dae5bcd0a7abe81fb61264d4
Theo <theo.meunier41@gmail.com>
2020-12-08 23:35:27 +0100

    feat:
    mise en place de la partie authentification, login

--- assets/app.js
+++ assets/app.js
@@ -6,8 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import './scss/app.scss';
+import './styles/app.scss';
 
 //import dependance
 import $ from 'jquery';
